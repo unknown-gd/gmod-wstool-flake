e276e97629ae7b6c9bab2ccb598ad011ddfd2ea4
Unknown Developer <olegis019@gmail.com>
2026-06-03 13:44:01 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,24 +14,36 @@
         url = "https://github.com/Srlion/gmod-wstool/releases/download/v${version}/gmod-wstool-${version}-1-x86_64.pkg.tar.zst";
         hash = "sha256-KwGsALcw24lZ1wVSAr5u+nuVKFTT8UHGwisKxX6YB90=";
       };
+
+      # Define runtime dependencies that need to be visible via LD_LIBRARY_PATH
+      runtimeLibs = with pkgs; [
+        wayland
+        libxkbcommon
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXi
+        vulkan-loader
+      ];
     in
     {
       packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
         pname = "gmod-wstool";
-        inherit version src; # Added src here so Nix knows what to unpack
+        inherit version src;
 
         nativeBuildInputs = with pkgs; [
           zstd
           gnutar
           autoPatchelfHook
+          makeWrapper # <--- Tool needed to create the environment wrapper
         ];
 
-        buildInputs = with pkgs; [
-          stdenv.cc.cc.lib
-          libgcc.lib
-        ];
+        buildInputs = [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.libgcc.lib
+        ]
+        ++ runtimeLibs;
 
-        # Removed dontUnpack = true; instead we write a pristine unpackPhase
         unpackPhase = ''
           tar -I zstd -xvf $src
         '';
@@ -39,13 +51,23 @@
         installPhase = ''
           mkdir -p $out/bin $out/lib $out/share
 
-          # Copying the directories extracted from the Arch package
           cp -r usr/lib/gmod-wstool $out/lib/
           cp -r usr/share/* $out/share/
 
-          # Symlink the binary to bin so it's in the user's $PATH
-          ln -s $out/lib/gmod-wstool/gmod-wstool $out/bin/gmod-wstool
+          # Wrap the actual binary to inject the runtime path for dlopen
+          makeWrapper $out/lib/gmod-wstool/gmod-wstool $out/bin/gmod-wstool \
+            --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeLibs}"
+
+          # Fix the desktop shortcut file accurately
+          if [ -f "$out/share/applications/gmod-wstool.desktop" ]; then
+            sed -i "s|^Exec=.*|Exec=$out/bin/gmod-wstool|" "$out/share/applications/gmod-wstool.desktop"
+          fi
         '';
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/gmod-wstool";
       };
     };
 }
